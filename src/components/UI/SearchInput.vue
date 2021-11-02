6ebea63f3a79ceb8e9bd3434da7ddfa4a62a871a
ryzzzin <rysinb@yandex.ru>
2021-11-02 18:20:08 +0600

    add(searchInput): create searchInput component

---
+++ src/components/UI/SearchInput.vue
@@ -0,0 +1,23 @@
+<template>
+    <input :value="modelValue" @input="updateInput" type="text" class="input">
+</template>
+
+<script>
+    export default {
+        name: 'search-input',
+        props: {
+            modelValue:{
+                type: [String, Number]
+            }
+        },
+        methods:{
+            updateInput(event){
+                this.$emit('update:modelValue', event.target.value)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
