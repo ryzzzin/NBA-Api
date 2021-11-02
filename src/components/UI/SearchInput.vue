302e604d290bbbcb8e08d9724706505c7f3478ee
ryzzzin <rysinb@yandex.ru>
2021-11-02 23:55:27 +0600

    edit(searchInput): added styles

--- src/components/UI/SearchInput.vue
+++ src/components/UI/SearchInput.vue
@@ -1,5 +1,8 @@
 <template>
-    <input :value="modelValue" @input="updateInput" type="text" class="input">
+    <div class="search">
+        <input :value="modelValue" @input="updateInput" type="text" class="input" placeholder="Search...">
+        <div class="hl-search"></div>
+    </div>
 </template>
 
 <script>
@@ -19,5 +22,30 @@
 </script>
 
 <style scoped>
+    .input{
+        color: #6a6a6a;
+        font-size: 18px;
+        font-weight: bold;
 
+        border: 0;
+        background: transparent;
+    }
+    
+    .input:focus{
+        outline: none;
+    }
+
+    .input::placeholder{
+        color: #a6a6a6;
+        font-size: 18px;
+        font-weight: bold;
+
+        border: 0;
+        background: transparent;
+    }
+
+    .hl-search {
+        border-top: 3px solid #6a6a6a;
+        width: 300px;
+    }
 </style>
