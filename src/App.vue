7187336026347db4b87f45aabf3ba5accdf2172a
ryzzzin <rysinb@yandex.ru>
2021-11-02 23:54:40 +0600

    edit(app): add teamsByYear, some cosmetic changes

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,14 @@
     <navbar></navbar>
     <div class="contents">
       <div class="contents__search">
-        <search-input v-model="searchQuery" placeholder="Search..."/>
+        <search-input v-model="searchQuery"/>
       </div>
       <div class="contents__header">
         <div class="page-name">Teams</div>
-        <sort-select v-model="selectedSort" :options="sortOptions" class="teams-sort"></sort-select>
+        <div class="teams-sort">
+           <sort-select v-model="selectedYear" :options="yearOptions" class="teams-sort"></sort-select>
+           <sort-select v-model="selectedSort" :options="sortOptions" class="teams-sort"></sort-select>
+        </div>
       </div>
       <teams-list
         :teams="sortedAndSearchedTeams"
@@ -36,9 +39,17 @@
     data(){
       return{
         teams:[],
-        isTeamLoading: false,
+        selectedYear: '2021',
         selectedSort: '',
         searchQuery: '',
+        yearOptions: [
+          {value: '2021', name: "2021"},
+          {value: '2020', name: "2020"},
+          {value: '2019', name: "2019"},
+          {value: '2018', name: "2018"},
+          {value: '2017', name: "2017"},
+          {value: '2016', name: "2016"}
+        ],
         sortOptions: [
           {value: 'tricode', name: "Tricode"},
           {value: 'confName', name: "Conference"},
@@ -57,22 +68,19 @@
           console.log(e);
         }
       },
-      async fetchTeams(){
+      async fetchTeams(year){
         try{
-          this.isTeamLoading = true;
-          const response = await axios.get('https://data.nba.net/data/10s/prod/v1/2021/teams.json');
+          const response = await axios.get('https://data.nba.net/data/10s/prod/v1/' + year + '/teams.json');
           this.teams = response.data.league.standard;
           console.log(this.teams);
         } catch(e){
           console.log(e);
-        } finally{
-          this.isTeamLoading = false;
         }
       }
     },
     mounted(){
       this.fetchPlayers();
-      this.fetchTeams();
+      this.fetchTeams('2021');
     },
     computed:{
       sortedTeams(){
@@ -80,6 +88,11 @@
       },
       sortedAndSearchedTeams(){
         return this.sortedTeams.filter(team => team.fullName.toLowerCase().includes(this.searchQuery.toLowerCase()))
+      }
+    },
+    watch:{
+      selectedYear(newValue){
+        this.fetchTeams(newValue);
       }
     }
   }
@@ -103,13 +116,23 @@
     margin-left: 330px;
     padding-top: 50px;
     padding-left: 50px;
+    display: flex;
+    flex-direction: column;
   }
+
+  .contents__search{
+    margin-right: 50px;
+    align-self: flex-end;
+  }
+
   .loading{
     margin: 50px;
     font-size: 24px;
   }
 
   .contents__header{
+    margin-top: 30px;
+
     display: flex;
     flex-direction: row;
     align-items: center;
