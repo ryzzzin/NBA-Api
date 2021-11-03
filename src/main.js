e2337a6b5934e527886b59e6b1a996d0dec56668
ryzzzin <rysinb@yandex.ru>
2021-11-03 22:13:46 +0600

    edit(main): added global UI components registration

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
-import router from './router/router';
 import { createApp } from 'vue'
-import App from './App'
+import App from '@/App'
+import components from '@/components/UI'
+import router from '@/router/router';
 
 const app = createApp(App)
+
+components.forEach(component => {
+    app.component(component.name, component)
+})
 
 app
 .use(router)
