88da249e675e527b1ebd97f8aa557e34cffd8bbf
ryzzzin <rysinb@yandex.ru>
2021-11-03 17:27:21 +0600

    add(main): create main page

---
+++ src/pages/Main.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>Welcome</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+h1{
+    margin-left: 500px;
+    margin-top: 100px;
+}
+</style>
