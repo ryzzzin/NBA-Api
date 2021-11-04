b178c5d4a2f1505f16bd439b5036b65674ffce2b
ryzzzin <rysinb@yandex.ru>
2021-11-04 19:14:27 +0600

    edit(main): added content on the main page

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -1,6 +1,20 @@
 <template>
-    <div>
-        <h1>Welcome</h1>
+    <div class="content__main">
+        <div class="header">What do you want to see?</div>
+        <div class="pages-links">
+            <router-link :to="'/players'" class="pages-link">
+                <img src="@/assets/svg/players.svg" class="page-img">
+                <div class="page-name">Players</div>
+            </router-link>
+            <router-link :to="'/teams'" class="pages-link">
+                <img src="@/assets/svg/teams.svg" class="page-img">
+                <div class="page-name">Teams</div>
+            </router-link>
+            <router-link :to="'/games'" class="pages-link">
+                <img src="@/assets/svg/games.svg" class="page-img">
+                <div class="page-name">Games</div>
+            </router-link>
+        </div>
     </div>
 </template>
 
@@ -11,5 +25,38 @@
 </script>
 
 <style scoped>
+    div{
+        color: #6a6a6a;
+    }
+    .header, .page-name{
+        font-size: 48px;
+        font-weight: bold;
+    }
+    .content__main{
+        height: 100vh;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
 
+    .pages-links{
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: flex-end;
+    }
+
+    .pages-link{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        margin: 5%;
+        text-decoration-color: #6a6a6a;
+    }
+
+    .page-img{
+        height: 27vh;
+    }
 </style>
