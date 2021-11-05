fdc53b5ad531ba1a4b4adba99089e87f2d3cea2c
ryzzzin <rysinb@yandex.ru>
2021-11-05 22:53:29 +0600

    add: global directives registration

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
 import { createApp } from 'vue'
 import App from '@/App'
 import components from '@/components/UI'
+import directives from '@/directives';
 import router from '@/router/router';
 
 const app = createApp(App)
 
 components.forEach(component => {
     app.component(component.name, component)
+})
+
+directives.forEach(directive => {
+    app.directive(directive.name, directive)
 })
 
 app
