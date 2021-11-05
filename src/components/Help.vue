19ad13cd1fd18c89c92bcd78ed474dce773b92f9
ryzzzin <rysinb@yandex.ru>
2021-11-05 23:01:59 +0600

    edit(help): restyled help button

--- src/components/Help.vue
+++ src/components/Help.vue
@@ -1,8 +1,5 @@
 <template>
-    <div class="help">
-        <div class="help__text">Any questions</div>
-        <div class="help__btn">?</div>
-    </div>
+    <a href="https://t.me/ryzzzin" target="_blank" class="help__btn">?</a>
 </template>
 
 <script>
@@ -10,47 +7,38 @@
 </script>
 
 <style scoped>
-.help{
-    width: 13vw;
-    height: 6vh;
-
-    position: fixed;
-    z-index: 1;
-    top: 91vh;
-    left: 86vw;
-    overflow-x: hidden;
-
-    background: rgba(246, 246, 246, 0.75);
-    box-shadow: 0 0 8px 8px rgba(246, 246, 246, 0.75);
-    border-radius: 80px;
-
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: center;
-
-    cursor: pointer;
-}
-
-.help__text{
-    margin-left: 15px;
-    color: #212121;
-    font-size: 24px;
-}
 
 .help__btn{
-    width: 3.2vw;
-    height: 5.5vh;
+    position: fixed;
+    z-index: 1;
+    bottom: 3%;
+    right: 2%;
+    overflow-x: hidden;
+
+    width: 60px;
+    height: 60px;
     background: #212121;
+    opacity: 90%;
     color: #f6f6f6;
     font-size: 30px;
 
     border: 0;
     border-radius: 50%;
-    cursor: pointer;
 
     display: flex;
     justify-content: center;
     align-items: center;
+    transition: 0.5s;
+
+    text-decoration: none;
+}
+
+.help__btn:hover{
+    font-size: 35px;
+    background: #464646;
+    opacity: 50%;
+    width: 70px;
+    height: 70px;
+    transition: 0.5s;
 }
 </style>
